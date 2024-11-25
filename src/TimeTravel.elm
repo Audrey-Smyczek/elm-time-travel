0afdd586f39efd1a8137aa440280e89af1478640
Audrey Smyczek <asmyczek@macalester.edu>
2024-11-25 14:43:12 -0600

    Pass Through Model Complete

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -3,3 +3,19 @@ module TimeTravel exposing (addTimeTravel)
 import Playground exposing (..)
 import Set
 
+addTimeTravel rawGame =
+    { initialState = initialStateWithTimeTravel rawGame
+    , updateState = updateWithTimeTravel rawGame
+    , view = viewWithTimeTravel rawGame
+    }
+
+initialStateWithTimeTravel rawGame =
+    rawGame.initialState
+    
+updateWithTimeTravel rawGame computer model =
+    rawGame.updateState computer model
+    
+viewWithTimeTravel rawGame computer model =
+    rawGame.view computer model
+
+
