3278776594dd7764e7059ab5d8d140d316140f39
Audrey Smyczek <asmyczek@macalester.edu>
2024-11-25 15:22:22 -0600

    Implement Pausing Error

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -10,12 +10,28 @@ addTimeTravel rawGame =
     }
 
 initialStateWithTimeTravel rawGame =
-    rawGame.initialState
+    { rawModel = rawGame.initialState
+    , paused = False
+    }
     
 updateWithTimeTravel rawGame computer model =
-    rawGame.updateState computer model
-    
+
+-- I am stuck on how to change paused to true/false since you can't reassign variables
+
+    if keyPressed "T" computer then
+        model.paused = True
+    else if keyPressed "R" computer then
+        model.paused = False
+    else if model.paused then
+        model 
+    else 
+        {model | rawModel = rawGame.updateState computer model.rawModel}
+
 viewWithTimeTravel rawGame computer model =
-    rawGame.view computer model
+    rawGame.view computer model.rawModel
 
-
+keyPressed keyName computer =
+  [ String.toLower keyName
+  , String.toUpper keyName
+  ]
+    |> List.any (\key -> Set.member key computer.keyboard.keys)
